d8f9835f40257be2ed823c4586de775305904dc8
dmytro boychuk <dimboychuk1995@gmail.com>
2016-04-20 08:00:43 +0300

    first commit

---
+++ src/COM/DIM/Main.java
@@ -0,0 +1,111 @@
+package COM.DIM;
+
+/**
+ * Created by us9522 on 19.04.2016.
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+// write your code here
+
+//Масив команд який оновлюється після кожного туру
+        Integer teams[][] = new Integer[6][10];
+//Масив який зберыгаэ результати передостаннього туру
+        Integer teamsPre[][] = new Integer[6][10];
+
+//проміжний масив для введення результатів
+        Integer tempResultsTeam[] = new Integer[6];
+        Integer tempResults[] = new Integer[6];
+
+        System.out.println("After 0st tour");
+//початкове присвоєння
+        System.out.println("Комавнда Тур Очки Матчі Виграші Нічиї Програші Різн.голів Забиті Пропущені");
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 10; j++){
+                teams[i][0] = i;
+                teamsPre[i][0] = i;
+                if (j > 0){
+                    teams[i][j] = 0;
+                    teamsPre[i][j] = 0;
+                }
+                tempResultsTeam[i] = teams[i][0];
+                System.out.print(teams[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("\n");
+
+        Scanner res = new Scanner(System.in);
+
+        System.out.println("\nвведіть всі результати туру:");
+//присвоєння результатів
+        for (int i = 0; i < tempResultsTeam.length; i++){
+            System.out.print(tempResultsTeam[i] + " ");
+            System.out.print("team" + tempResultsTeam[i] + ":");
+            tempResults[i] = res.nextInt();
+        }
+//виведення результатів матчів(між собою грають сусідні команди)
+        System.out.println("\nteam id");
+        for(int i = 0; i < tempResults.length; i++){
+            System.out.print(tempResultsTeam[i] + " ");
+        }
+        System.out.println("\nresult");
+        for(int i = 0; i < tempResults.length; i++){
+            System.out.print(tempResults[i] + " ");
+        }
+
+//заповнення головної таблиці після 1 туру
+        System.out.println("\n\nAfter 1st tour");
+        System.out.println("\nКомавнда Тур Очки Матчі Виграші Нічиї Програші Різн.голів Забиті Пропущені\n");
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 10; j++){
+                teams[i][1] = 1;
+                teams[i][3] = 1;
+
+                //забиті
+                teams[i][8] = tempResults[i];
+
+                //пропущені
+                //для команди з парним та нульовим індексом
+                if ((i == 0) | (i % 2 == 0)){
+                    teams[i][9] = tempResults[i + 1];
+                //дальше для команд з іншими індексами
+                }else {
+                    teams[i][9] = tempResults[i - 1];
+                }
+
+                //різниця
+                teams[i][7] = teams[i][8] - teams[i][9];
+
+                //Виграші, нічиї та програші для команд з нульовим та парним індексом
+                if (i == 0 || i % 2 == 0){
+                    if(tempResults[i] > tempResults[i + 1]){
+                        teams[i][4] = teamsPre[i][4] + 1;
+                    }else if(tempResults[i] < tempResults[i + 1]){
+                        teams[i][6] = teamsPre[i][6] + 1;
+                    }else{
+                        teams[i][5] = teamsPre[i][5] + 1;
+                    }
+                //Виграші, нічиї та програші для команд з непарним індексом
+                }else{
+                    if(tempResults[i - 1] < tempResults[i]){
+                        teams[i][4] = teamsPre[i][4] + 1;
+                    }else if (tempResults[i - 1] > tempResults[i]){
+                        teams[i][6] = teamsPre[i][6] + 1;
+                    }else{
+                        teams[i][5] = teamsPre[i][5] + 1;
+                    }
+                }
+
+                //В кінцевому результаті рахуємо очки без бонусних
+                teams[i][2] = (teams[i][4] * 3) + (teams[i][6]);
+
+                System.out.print(teams[i][j] + "   ");
+            }
+            System.out.println("");
+        }
+    }
+}
